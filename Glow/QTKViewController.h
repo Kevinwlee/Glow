5eb9d19047def63ad3d6f420267cf4f4947b7618
Kevin W. Lee <kevinwarrenlee@gmail.com>
2012-08-15 07:20:33 -0500

    Initial Commit

---
+++ Glow/QTKViewController.h
@@ -0,0 +1,13 @@
+//
+//  QTKViewController.h
+//  Glow
+//
+//  Created by Kevin Lee on 8/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QTKViewController : UIViewController
+
+@end
---
+++ Glow/QTKViewController.m
@@ -0,0 +1,34 @@
+//
+//  QTKViewController.m
+//  Glow
+//
+//  Created by Kevin Lee on 8/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "QTKViewController.h"
+
+@interface QTKViewController ()
+
+@end
+
+@implementation QTKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
